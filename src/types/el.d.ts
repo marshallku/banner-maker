b280988e597149de70ede591bc0a509e4bae1375
marshallku <marshall@kakao.com>
2022-05-11 20:03:37 +0900

    Add eln to create SVG Element

--- src/types/el.d.ts
+++ src/types/el.d.ts
@@ -31,3 +31,37 @@ type CustomElementAttributes<T extends CustomElementKeys> = Partial<
     CreatedElement<T>
 > &
     Partial<CustomEventObject<T>>;
+
+interface CustomSVGElements extends SVGElementTagNameMap {}
+
+type CustomSVGElementKeys = keyof CustomSVGElements;
+
+type CreatedSVGElement<T extends CustomSVGElementKeys> = CustomSVGElements[T];
+
+type CustomSVGElementEventListener<T extends CustomSVGElementKeys> = Pick<
+    CreatedSVGElement<T>,
+    "addEventListener"
+>;
+
+type CustomSVGElementEventListenerParameter<T extends CustomSVGElementKeys> =
+    Parameters<CreatedElement<T>["addEventListener"]>;
+
+type CustomSVGEvents<T extends CustomSVGElementKeys> = {
+    [key in CustomSVGElementEventListenerParameter<T>[0]]:
+        | CustomSVGElementEventListenerParameter<T>[1]
+        | [
+              CustomSVGElementEventListenerParameter<T>[1],
+              CustomSVGElementEventListenerParameter<T>[2]
+          ];
+};
+
+interface CustomSVGEventObject<T extends CustomSVGElementKeys> {
+    events: Partial<CustomSVGEvents<T>>;
+}
+
+interface CustomSVGElementInterface {
+    [key: string]: string;
+}
+
+type CustomSVGElementAttributes<T extends CustomSVGElementKeys> =
+    CustomSVGElementInterface & Partial<CustomSVGEventObject<T>>;
--- src/utils/el.ts
+++ src/utils/el.ts
@@ -66,3 +66,63 @@ export default function el<
 
     return node as U;
 }
+
+export function eln<
+    T extends CustomSVGElementKeys,
+    U extends CreatedSVGElement<T>
+>(
+    nodeName: T,
+    attributes: CustomSVGElementAttributes<T> = {},
+    ...children: Array<string | Element | undefined | null>
+): U {
+    const node = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        nodeName
+    );
+
+    children.forEach((childNode) => {
+        if (!childNode) {
+            return;
+        }
+
+        if (childNode instanceof Element) {
+            node.appendChild(childNode);
+            return;
+        }
+
+        node.appendChild(document.createTextNode(childNode));
+    });
+
+    Object.entries(attributes).forEach(([key, value]) => {
+        if (key === "events") {
+            Object.entries(value as Partial<CustomSVGEvents<T>>).forEach(
+                ([type, args]) => {
+                    if (Array.isArray(args)) {
+                        const curArgs = args as [
+                            EventListenerOrEventListenerObject,
+                            boolean | AddEventListenerOptions | undefined
+                        ];
+
+                        node.addEventListener(type, ...curArgs);
+
+                        return;
+                    }
+
+                    const curArgs = args as EventListenerOrEventListenerObject;
+
+                    node.addEventListener(type, curArgs);
+                }
+            );
+
+            return;
+        }
+
+        if (key === "className") {
+            node.classList.add(value);
+        }
+
+        node.setAttribute(key, value as string);
+    });
+
+    return node as U;
+}
