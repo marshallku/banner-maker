e64d88adda5830a4bd6113388f7131ca8061f271
marshallku <marshall@kakao.com>
2022-05-12 17:04:26 +0900

    Move InputProps to d.ts

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -7,13 +7,7 @@ export default function Input({
     className,
     type = "text",
     value,
-}: {
-    title: string;
-    id?: string;
-    className?: string;
-    type?: string;
-    value?: string;
-}) {
+}: InputProps) {
     return el(
         "div",
         { className: fcls("inputWrap", className) },
---
+++ src/types/Input.d.ts
@@ -0,0 +1,7 @@
+interface InputProps {
+    title: string;
+    id?: string;
+    className?: string;
+    type?: string;
+    value?: string;
+}
