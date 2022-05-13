fab81e05e2ccab116855880c4e45e18c90046c46
marshallku <marshall@kakao.com>
2022-05-13 14:45:06 +0900

    Simplify getRandomColor

--- src/utils/getRandomColor.ts
+++ src/utils/getRandomColor.ts
@@ -1,5 +1,3 @@
 export default function getRandomColor() {
-    return `#${(0x1000000 + Math.random() * 0xffffff)
-        .toString(16)
-        .substring(0, 6)}`;
+    return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 }
